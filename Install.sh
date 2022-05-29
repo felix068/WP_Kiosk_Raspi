f051c325439c713f76db946d254524722b03b658
felix068 <77407722+felix068@users.noreply.github.com>
2022-05-29 18:35:40 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -19,9 +19,9 @@ confirm()
 }
 
 if confirm "Are you sure to install the program?"; then
-    sudo apt-get update & sudo apt-get full-upgrade
+    sudo apt-get update & sudo apt-get full-upgrade -y
     echo "The software will install..."
-    sudo apt-get install xserver-xorg-input-evdev xinput-calibrator xorg unclutter chromium-browser
+    sudo apt-get install xserver-xorg-input-evdev xinput-calibrator xorg unclutter chromium-browser -y
     sudo cp -rf /usr/share/X11/xorg.conf.d/10-evdev.conf /usr/share/X11/xorg.conf.d/45-evdev.conf
     sudo rm /home/pi/.profile
     sudo rm /home/pi/.xinitrc
@@ -30,8 +30,15 @@ if confirm "Are you sure to install the program?"; then
     sudo wget https://raw.githubusercontent.com/felix068/Work_Raspi_Kiosk/main/.xinitrc
     cd /
     sudo wget https://raw.githubusercontent.com/felix068/Work_Raspi_Kiosk/main/st.sh
-    sudo nano /etc/X11/xorg.conf.d/98-dietpi-disable_dpms.conf
+    echo "Setting your config file"
+    sleep 2
     sudo nano /boot/config.txt
+    echo "Setting Xorg (X11)"
+    sleep 2
+    sudo nano /etc/X11/xorg.conf.d/98-dietpi-disable_dpms.conf
+    echo "Setting your screen resolution and chromium argument"
+    sleep 2
+    sudo nano /home/pi/.xinitrc
     echo "The operation was done !"
 else
     echo "The operation was canceled by the user."
