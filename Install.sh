a2eb3a856b9dee52eabb8c4ddaa0c03ae31b6dda
felix068 <77407722+felix068@users.noreply.github.com>
2022-05-29 18:55:04 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -26,10 +26,10 @@ if confirm "Are you sure to install the program?"; then
     sudo rm /home/pi/.profile
     sudo rm /home/pi/.xinitrc
     cd /home/pi/
-    sudo wget https://raw.githubusercontent.com/felix068/Work_Raspi_Kiosk/main/.profile
-    sudo wget https://raw.githubusercontent.com/felix068/Work_Raspi_Kiosk/main/.xinitrc
+    sudo wget https://raw.githubusercontent.com/felix068/Working_Raspi_Kiosk/main/.profile
+    sudo wget https://raw.githubusercontent.com/felix068/Working_Raspi_Kiosk/main/.xinitrc
     cd /
-    sudo wget https://raw.githubusercontent.com/felix068/Work_Raspi_Kiosk/main/st.sh
+    sudo wget https://raw.githubusercontent.com/felix068/Working_Raspi_Kiosk/main/st.sh
     echo "Setting your config file"
     sleep 2
     sudo nano /boot/config.txt
